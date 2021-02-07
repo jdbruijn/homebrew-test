83cc0178555ab55b6c051aa4b39d0d5306869883
Jeroen de Bruijn <62570005+jdbruijn@users.noreply.github.com>
2021-02-07 13:59:58 +0000

    gothanks: update 0.3.0 bottle.

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -7,8 +7,8 @@ class Gothanks < Formula
 
   bottle do
     root_url "https://github.com/jdbruijn/homebrew-test/releases/download/gothanks-0.3.0"
-    sha256 cellar: :any_skip_relocation, catalina:     "c1bb5614f7103d4176a9d98bee8fe7d9d70755d42d0d19e8d8c312c63b53365f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "48dc2bc15c5fe703cc2895fda4596707d816a89b10960ee8fdae2d735bcc894a"
+    sha256 cellar: :any_skip_relocation, catalina:     "ece3d82a126f820e609275f254fd39f956200f51e2c8c79c0e6af2c3c3204172"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3d4ad3512c37678e0d7a3f86f7f3d1c08f932f2c7073ddd5a6a058d30c6f41ab"
   end
 
   depends_on "go" => :build
