bdd026931cba3da356f9433a5504841f9383ccec
Jeroen de Bruijn <vv9239@gmail.com>
2021-02-07 11:48:15 +0100

    feat: add cmake 3.15.5, which I expect to fail due to system deps

---
+++ Formula/cmake@3.15.5.rb
@@ -0,0 +1,51 @@
+class CmakeAT3155 < Formula
+  desc "Cross-platform make"
+  homepage "https://www.cmake.org/"
+  url "https://github.com/Kitware/CMake/releases/download/v3.15.5/cmake-3.15.5.tar.gz"
+  version "0.1.0"
+  sha256 "fbdd7cef15c0ced06bb13024bfda0ecc0dedbcaaaa6b8a5d368c75255243beb4"
+  license "BSD-3-Clause"
+  head "https://gitlab.kitware.com/cmake/cmake.git"
+
+  keg_only :versioned_formula
+
+  depends_on "sphinx-doc" => :build
+
+  # The completions were removed because of problems with system bash
+
+  # The `with-qt` GUI option was removed due to circular dependencies if
+  # CMake is built with Qt support and Qt is built with MySQL support as MySQL uses CMake.
+  # For the GUI application please instead use `brew cask install cmake`.
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --no-system-libs
+      --parallel=#{ENV.make_jobs}
+      --datadir=/share/cmake
+      --docdir=/share/doc/cmake
+      --mandir=/share/man
+      --sphinx-build=#{Formula["sphinx-doc"].opt_bin}/sphinx-build
+      --sphinx-html
+      --sphinx-man
+      --system-zlib
+      --system-bzip2
+      --system-curl
+    ]
+
+    # There is an existing issue around macOS & Python locale setting
+    # See https://bugs.python.org/issue18378#msg215215 for explanation
+    ENV["LC_ALL"] = "en_US.UTF-8"
+
+    system "./bootstrap", *args, "--", "-DCMAKE_BUILD_TYPE=Release"
+    system "make"
+    system "make", "install"
+
+    elisp.install "Auxiliary/cmake-mode.el"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
+    system bin/"cmake", "."
+  end
+end
