b345529c6e6e74899be48e7b9e05fbd8a3d51cc8
Jeroen de Bruijn <vv9239@gmail.com>
2021-02-07 15:38:35 +0100

    fix: bump all

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -2,8 +2,11 @@ class Gothanks < Formula
   desc "Automatically star your go.mod Github dependencies"
   homepage "https://github.com/psampaz/gothanks"
   url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
+  version "0.1.0"
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
   license "MIT"
+  
+  # Bump comment
 
   bottle do
     root_url "https://github.com/jdbruijn/homebrew-test/releases/download/gothanks-0.3.0"
